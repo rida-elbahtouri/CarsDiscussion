0ba07a798c07dd91931e4da16af02674448fab84
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-08-12 16:48:11 +0100

    create the sign in ,sign up functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
     has_many :followers,class_name:'Following',foreign_key:'follower_id'
     has_many :followeds,class_name:'Following',foreign_key:'followed_id'
 
+    has_one_attached :photo
+    has_one_attached :cover_image
+
     validates :username, presence: true, uniqueness: true
     validates :fullname, presence: true
 end
